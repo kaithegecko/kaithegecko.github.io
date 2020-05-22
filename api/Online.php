4934f025796078fb61b921018466d3dbfd6639ed
Kyila <49961714+kaithegecko@users.noreply.github.com>
2020-05-22 10:07:59 -0400

    Create Online.php

---
+++ api/Online.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+  $xml = file_get_contents("http://www.example.com/file.xml");
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
