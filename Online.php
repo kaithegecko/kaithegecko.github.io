bc67a5f82273d3f06e42fd2bea806bcd83e714a4
Kyila <49961714+kaithegecko@users.noreply.github.com>
2020-08-13 16:32:20 -0400

    Delete Online.php

--- Online.php
+++
@@ -1,9 +0,0 @@
-<html>
- <head>
-  <title>PHP Test</title>
-  $xml = file_get_contents("http://www.example.com/file.xml");
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
-</html>
